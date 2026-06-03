7683e397ea727480de0064272b8a75175b4170cb
Aariz Sattar <sattara2@ad.hva.nl>
2026-06-03 13:55:50 +0200

    feat: added test files for qrcode module and service

---
+++ tests/server/qrcode-module.test.ts
@@ -0,0 +1,19 @@
+/**
+ * @file qrcode-module.test.ts
+ */
+import { describe, it } from 'node:test'
+import assert from 'node:assert/strict'
+import { QrcodeModule } from '../../src/server/qrcode/qrcode.module.js'
+import { QrcodeService } from '../../src/server/qrcode/qrcode.service.js'
+
+describe('QrcodeModule', () => {
+  it('exports the module', () => {
+    assert.ok(QrcodeModule)
+    assert.strictEqual(QrcodeModule.name, 'QrcodeModule')
+  })
+
+  it('exports QrcodeService', () => {
+    assert.ok(QrcodeService)
+    assert.strictEqual(typeof QrcodeService, 'function')
+  })
+})
