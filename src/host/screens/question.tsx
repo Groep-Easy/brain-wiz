0d6a80e223ccc1c9a56fe325ab24cbecd932775e
d.eeninkwinkel <daniel.eeninkwinkel@student.uva.nl>
2026-06-04 22:54:23 +0200

    h

--- src/host/screens/question.tsx
+++ src/host/screens/question.tsx
@@ -23,10 +23,10 @@ export function Question() {
   const socketRef = useRef<WebSocket | null>(null)
   const idRef = useRef(0)
 
-  var gameCode = "12345"
-  var theme = "Geography"
-  var currentQuestion = "What is the highest building in the world?"
-  var answers = ["Burj Khalifa", "Taipei 101", "One World Trade Center", "woidjiew"]
+  var gameCode = '12345'
+  var theme = 'Geography'
+  var currentQuestion = 'What is the highest building in the world?'
+  var answers = ['Burj Khalifa', 'Taipei 101', 'One World Trade Center', 'woidjiew']
   var amountAnswers = 5
   var timer = 29
 
@@ -114,8 +114,8 @@ export function Question() {
     let rawData = JSON.parse(raw)
     append('in', raw + rttNote(raw))
 
-    const {event, data} = rawData
-    switch(event) {
+    const { event, data } = rawData
+    switch (event) {
       case 'roomCreated':
         setCode(data.code)
         setHostToken(data.hostToken)
@@ -142,22 +142,28 @@ export function Question() {
   return (
     <main className="host-question-page">
       <div className="top">
-        <div className="top-left"><p>Game code: {gameCode}</p></div>
+        <div className="top-left">
+          <p>Game code: {gameCode}</p>
+        </div>
         <div className="top-center">{theme} quiz</div>
         <div className="top-right">
           <button>Toggle sound</button>
           <button>Settings</button>
         </div>
       </div>
-      <div className="question"><h1>{currentQuestion}</h1></div>
-      <div className="image"><img></img></div>
+      <div className="question">
+        <h1>{currentQuestion}</h1>
+      </div>
+      <div className="image">
+        <img></img>
+      </div>
       <div className="imageanswer"> </div>
       <div className="answers">
         <div className="answer answer1">{answers[0]}</div>
         <div className="answer answer2">{answers[1]}</div>
         <div className="answer answer3">{answers[2]}</div>
         <div className="answer answer4">{answers[3]}</div>
-    </div>
+      </div>
       <div className="bottom">
         <p>{amountAnswers}</p>
         <p>{timer}s left!</p>
