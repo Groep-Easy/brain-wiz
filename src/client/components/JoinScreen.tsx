ed9d01d5fa3ab8892d34aa563560d5d41b696e5c
ldtorrejon <alvarodurantorrejon@gmail.com>
2026-06-05 01:32:15 +0200

    improve join screen layout and add styling

--- src/client/components/JoinScreen.tsx
+++ src/client/components/JoinScreen.tsx
@@ -2,11 +2,16 @@ export function JoinScreen({ onJoin }: { onJoin: () => void }) {
   return (
     <div className="screen">
       <h2>Join game</h2>
-      <label>Set name: </label>
-      <input type="text" id="name"></input><br></br>
-      <label>Room code: </label>
-      <input type="text" id="room"></input><br></br>
-      <button onClick={onJoin}>Join room</button>
+
+      <div className="join-form">
+        <label htmlFor="name">Set name</label>
+        <input type="text" id="name" />
+
+        <label htmlFor="room">Room code</label>
+        <input type="text" id="room" />
+
+        <button onClick={onJoin}>Join room</button>
+      </div>
     </div>
   )
 }
