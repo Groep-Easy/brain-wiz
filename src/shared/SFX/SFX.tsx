9139c5eac04cb9fa1d27b231cb5544809751c010
d.eeninkwinkel <daniel.eeninkwinkel@student.uva.nl>
2026-06-19 14:05:27 +0200

    sound module update

--- src/shared/SFX/SFX.tsx
+++ src/shared/SFX/SFX.tsx
@@ -1,13 +1,17 @@
-export const SOUNDS = Object.freeze({
-  cardDrop: './card-drop.mp3',
-  correct: './correct.mp3',
-  die: './die.mp3',
-  partyHorn: './party-horn.mp3',
-  pop: './pop.mp3',
-  quack: './quack.mp3',
-  startGame: './start-game.mp3',
-  waterDrop: './water-drop.mp3',
-})
+//import jazz from './jazz.mp3'
+import gameOver from './synthwave.mp3'
+import suspense from './Standoff.mp3'
+
+import cardDrop from './card-drop.mp3'
+import correct from './correct.mp3'
+import die from './die.mp3'
+import roundIntro from './intro.wav'
+import leaderBoard from './leaderboard.mp3'
+import partyHorn from './party-horn.mp3'
+import pop from './pop.mp3'
+import quack from './quack.mp3'
+import startGame from './start-game.wav'
+import waterDrop from './water-drop.mp3'
 
 export function playSound(soundSource: string) {
   const audioData = new Audio(soundSource)
@@ -16,4 +20,52 @@ export function playSound(soundSource: string) {
   audioData.play().catch(() => {
     /* ignore playback/autoplay errors */
   })
+}
+
+export function gameOverSound() {
+  playSound(gameOver)
+}
+
+export function suspenseSound() {
+  playSound(suspense)
+}
+
+export function cardDropSound() {
+  playSound(cardDrop)
+}
+
+export function correctSound() {
+  playSound(correct)
+}
+
+export function dieSound() {
+  playSound(die)
+}
+
+export function roundIntroSound() {
+  playSound(roundIntro)
+}
+
+export function leaderBoardSound() {
+  playSound(leaderBoard)
+}
+
+export function partyHornSound() {
+  playSound(partyHorn)
+}
+
+export function popSound() {
+  playSound(pop)
+}
+
+export function quackSound() {
+  playSound(quack)
+}
+
+export function startGameSound() {
+  playSound(startGame)
+}
+
+export function waterDropSound() {
+  playSound(waterDrop)
 }
